3cae071d966f8769450896e9efd345494226cee6
Joao Marcus Cardoso <rfcjoujou1@gmail.com>
2018-07-12 23:24:36 -0300

    Criamos um jogo para que o uśuario acerte o número

---
+++ script.js
@@ -0,0 +1,17 @@
+function verificar() {
+	var n1 = document.getElementById("n1").innerHTML;
+	var n2 = document.getElementById("n2").value;
+	if (n1 == n2) {
+		alert("VOCÊ ACERTOU O NÚMERO!");
+	}else {
+		alert("VOCÊ ERROU O NÚMERO");
+	}
+
+	reseta();
+}
+function reseta() {
+	document.getElementById("n2").value = "" ;
+
+	var r = Math.floor(Math.random() * 100);
+	document.getElementById("n1").innerHTML = r;
+}
